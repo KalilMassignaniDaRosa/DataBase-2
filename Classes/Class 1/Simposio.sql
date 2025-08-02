0a6f16ecebb3b473d0504eb9874c95e2f56758d5
KalilMassignaniDaRosa <kalil.kmr@Gmail.com>
2025-08-01 21:56:12 -0300

    First Commit

---
+++ Classes/Class 1/Simposio.sql
@@ -0,0 +1,192 @@
+IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = 'atividadeSimposio')
+BEGIN
+    CREATE DATABASE atividadeSimposio;
+END
+
+USE atividadeSimposio;
+
+CREATE TABLE Pessoa(
+	id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	cpf VARCHAR(11) NOT NULL UNIQUE,
+	nome_completo VARCHAR(255) NOT NULL,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	telefone VARCHAR(15) NOT NULL UNIQUE,
+	sexo VARCHAR(1) NOT NULL CHECK(sexo IN('M','F')),
+	data_nascimento DATE NOT NULL,
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    codigo_universitario INT DEFAULT NULL,
+    eh_universitario BIT NOT NULL DEFAULT 0,
+
+	tipo_pessoa VARCHAR(12) NOT NULL DEFAULT 'Participante' CHECK (tipo_pessoa IN ('Participante', 'Palestrante', 'Organizador')),
+    -- Impedindo que eh_universitario seja true e o codigo_universitario esteja vazio
+    CONSTRAINT chk_codigo_universitario
+		CHECK(
+			(eh_universitario = 1 AND codigo_universitario IS NOT NULL)
+            OR 
+            (eh_universitario = 0 AND codigo_universitario IS NULL)
+        )
+);
+
+CREATE TABLE Tema(
+	id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	nome VARCHAR(255) NOT NULL,
+	descricao TEXT NOT NULL,
+	area_conhecimento VARCHAR(255) NOT NULL, 
+	data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE Comissao(
+	id  INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	nome VARCHAR(255) NOT NULL,
+	id_tema INT NOT NULL,
+	descricao TEXT,
+    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	CONSTRAINT Fk_comissao_tema FOREIGN KEY (id_tema) REFERENCES Tema(id)
+);
+
+CREATE TABLE Minicurso(
+	id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	titulo VARCHAR(255) NOT NULL,
+	data_evento DATE NOT NULL,
+	hora_inicio TIME NOT NULL,
+	hora_fim TIME NOT NULL,
+    id_tema INT NOT NULL,
+    CONSTRAINT FK_minicurso_tema FOREIGN KEY (id_tema) REFERENCES Tema(id)
+);
+
+CREATE TABLE Artigo(
+	id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	avaliacao VARCHAR(255) NOT NULL,
+	titulo VARCHAR(255) NOT NULL,
+	data_submissao DATE NOT NULL,
+	data_avaliacao DATE,
+	nota_avaliacao INT,
+	id_comissao  INT NOT NULL,
+    id_tema INT NOT NULL,
+	CONSTRAINT Fk_artigo_comissao FOREIGN KEY (id_comissao) REFERENCES Comissao(id),
+    CONSTRAINT Fk_artigo_tema FOREIGN KEY (id_tema) REFERENCES Tema(id)
+);
+
+CREATE TABLE Palestra(
+	id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	titulo VARCHAR(255) NOT NULL,
+	data_evento DATE NOT NULL,
+	hora_inicio TIME NOT NULL,
+	hora_fim TIME NOT NULL,
+    id_tema INT NOT NULL,
+    CONSTRAINT Fk_palestra_tema FOREIGN KEY (id_tema) REFERENCES Tema(id)
+);
+
+CREATE TABLE Simposio(
+	id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	nome VARCHAR(255) NOT NULL,
+	descricao TEXT,
+	universidade VARCHAR(255) NOT NULL,
+	data_inicio DATE NOT NULL,
+	data_fim DATE NOT NULL,
+	id_orador_principal INT NOT NULL,
+	local_simposio VARCHAR(255) NOT NULL,
+	capacidade INT,
+	CONSTRAINT Fk_orador_simposio FOREIGN KEY (id_orador_principal) REFERENCES Pessoa(id)
+);
+
+-- Tabelas de Relacionamentos
+CREATE TABLE Comissao_Pessoa(
+	id_pessoa INT NOT NULL,
+	id_comissao  INT NOT NULL,
+	PRIMARY KEY (id_pessoa, id_comissao),
+	CONSTRAINT Fk_comissao_id_pessoa FOREIGN KEY (id_pessoa) REFERENCES Pessoa(id),
+	CONSTRAINT Fk_comissao_id_comissao FOREIGN KEY (id_comissao) REFERENCES Comissao(id)
+);
+
+CREATE TABLE Inscrito_Simposio(
+	id_evento INT NOT NULL,
+	id_pessoa INT NOT NULL,
+    data_inscricao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (id_pessoa, id_evento),
+	CONSTRAINT Fk_inscrito_pessoa FOREIGN KEY (id_pessoa) REFERENCES Pessoa(id),
+	CONSTRAINT Fk_inscrito_simposio FOREIGN KEY (id_evento) REFERENCES Simposio(id)
+);
+
+CREATE TABLE Minicurso_Simposio(
+	id_simposio INT NOT NULL,
+	id_minicurso INT NOT NULL,
+	PRIMARY KEY (id_simposio, id_minicurso),
+	CONSTRAINT Fk_minicurso_simposio FOREIGN KEY (id_simposio) REFERENCES Simposio(id),
+	CONSTRAINT Fk_simposio_minicurso FOREIGN KEY (id_minicurso) REFERENCES Minicurso(id)
+);
+
+CREATE TABLE Artigo_Simposio(
+	id_simposio INT NOT NULL,
+	id_artigo INT NOT NULL,
+	PRIMARY KEY (id_simposio, id_artigo),
+	CONSTRAINT Fk_artigo_simposio FOREIGN KEY (id_simposio) REFERENCES Simposio(id),
+	CONSTRAINT Fk_simposio_artigo FOREIGN KEY (id_artigo) REFERENCES Artigo(id)
+);
+
+CREATE TABLE Palestra_Simposio(
+	id_simposio INT NOT NULL,
+	id_palestra INT NOT NULL,
+	PRIMARY KEY (id_simposio, id_palestra),
+	CONSTRAINT Fk_simposio_palestra FOREIGN KEY (id_simposio) REFERENCES Simposio(id),
+	CONSTRAINT Fk_palestra_simposio FOREIGN KEY (id_palestra) REFERENCES Palestra(id)
+);
+
+CREATE TABLE Organizador_Simposio (
+	id_pessoa INT NOT NULL,
+	id_simposio INT NOT NULL,
+	PRIMARY KEY (id_pessoa, id_simposio),
+	CONSTRAINT Fk_organizador_simposio_pessoa FOREIGN KEY (id_pessoa) REFERENCES Pessoa(id),
+	CONSTRAINT Fk_organizador_simposio_simposio FOREIGN KEY (id_simposio) REFERENCES Simposio(id)
+);
+
+CREATE TABLE Autor_Artigo(
+	id_pessoa INT NOT NULL,
+	id_artigo INT NOT NULL,
+	PRIMARY KEY (id_pessoa, id_artigo),
+	CONSTRAINT Fk_autor_artigo_pessoa FOREIGN KEY (id_pessoa) REFERENCES Pessoa(id),
+	CONSTRAINT Fk_autor_artigo_artigo  FOREIGN KEY (id_artigo) REFERENCES Artigo(id)
+);
+
+CREATE TABLE Inscricao_Minicurso(
+	id_pessoa INT NOT NULL,
+	id_minicurso INT NOT NULL,
+    data_inscricao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (id_pessoa, id_minicurso),
+	CONSTRAINT Fk_inscricao_minicurso_pessoa FOREIGN KEY (id_pessoa) REFERENCES Pessoa(id),
+	CONSTRAINT Fk_inscricao_minicurso_minicurso FOREIGN KEY (id_minicurso) REFERENCES Minicurso(id)
+);
+
+CREATE TABLE Inscricao_Palestra(
+	id_pessoa INT NOT NULL,
+	id_palestra INT NOT NULL,
+    data_inscricao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (id_pessoa, id_palestra),
+	CONSTRAINT Fk_inscricao_palestra_pessoa FOREIGN KEY (id_pessoa) REFERENCES Pessoa(id),
+	CONSTRAINT Fk_inscricao_palestra_palestra FOREIGN KEY (id_palestra) REFERENCES Palestra(id)
+);
+
+CREATE TABLE Inscricao_Artigo (
+	id_pessoa INT NOT NULL,
+	id_artigo INT NOT NULL,
+    data_inscricao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id_pessoa, id_artigo),
+    CONSTRAINT Fk_ia_pessoa FOREIGN KEY (id_pessoa) REFERENCES Pessoa(id),
+    CONSTRAINT Fk_ia_artigo FOREIGN KEY (id_artigo) REFERENCES Artigo(id)
+);
+
+CREATE TABLE Orador_Minicurso (
+	id_minicurso INT NOT NULL,
+	id_pessoa INT NOT NULL,
+	PRIMARY KEY (id_minicurso, id_pessoa),
+	CONSTRAINT Fk_orador_minicurso  FOREIGN KEY (id_minicurso) REFERENCES Minicurso(id),
+	CONSTRAINT Fk_om_pessoa  FOREIGN KEY (id_pessoa) REFERENCES Pessoa(id)
+);
+
+CREATE TABLE Orador_Palestra (
+	id_palestra INT NOT NULL,
+	id_pessoa INT NOT NULL,
+	PRIMARY KEY (id_palestra, id_pessoa),
+	CONSTRAINT Fk_orador_palestra FOREIGN KEY (id_palestra) REFERENCES Palestra(id),
+	CONSTRAINT Fk_op_pessoa  FOREIGN KEY (id_pessoa) REFERENCES Pessoa(id)
+);
