cd3685d878b08e8d46e483ff32b23aaf509ba2d0
KalilMassignaniDaRosa <kalil.kmr@Gmail.com>
2025-10-31 21:56:57 -0300

    Dapper

--- Classes/Class 3/Scaffolding/Scaffolding/Controllers/HomeController.cs
+++ Classes/Class 3/Scaffolding/Scaffolding/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
+using Dapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using Scaffolding.Models;
 
 namespace Scaffolding.Controllers
@@ -7,10 +9,32 @@ namespace Scaffolding.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _config;
+        private readonly string _connectionString;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(
+            ILogger<HomeController> logger,
+            IConfiguration config)
         {
             _logger = logger;
+            _config = config;
+            // Retorna um dicionario
+            _connectionString = _config.GetSection("ConnectionStrings")["DefaultConnection"]!;
+        }
+
+        [HttpGet]
+        public IActionResult Dapper()
+        {
+            using (
+                SqlConnection connection = new (_connectionString)
+            )
+            {
+                var data = connection.Query<Student>(
+                    "SELECT *" +
+                    "FROM Student");
+
+                return View(data);
+            }
         }
 
         public IActionResult Index()
